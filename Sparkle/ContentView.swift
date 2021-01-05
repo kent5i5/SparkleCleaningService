10cf0fed2b49ce0db52c4cce45fc11b6b7bcbcf2
yinkin <oceanamng@hotmail.com>
2021-01-05 11:27:58 -0800

    Initial Commit

---
+++ Sparkle/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sparkle
+//
+//  Created by ying kit ng on 1/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
